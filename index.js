71dfbfe70a8012f035d01f507a8df992b9b58812
Ezekielle Louise Marie L. Gambong <104586559+EzekielleGambong@users.noreply.github.com>
2023-12-15 12:50:48 +0800

    -DONE

--- index.js
+++ index.js
@@ -1,11 +1,47 @@
 const express = require('express');
-const routes = require('./routes.js/users');
+const jwt = require('jsonwebtoken');
+const session = require('express-session')
+const customer_routes = require('./router/auth_users.js').authenticated;
+const genl_routes = require('./router/general.js').general;
 
 const app = express();
-const PORT =5000;
 
 app.use(express.json());
 
-app.use("/user", routes);
+app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
-app.listen(PORT,()=>console.log("Server is running at port "+PORT));
+app.use("/customer/auth/*", function auth(req,res,next) {
+    // if the authorization exists there was a log in event
+    if (req.session.authorization) {
+        // get the session token
+        token = req.session.authorization['accessToken'];
+        // verify the token
+        jwt.verify(token, "access", (err, user) => {
+            if (!err) {
+                // cache the user object in the request
+                req.user = user;
+                next();
+            }
+            // the token has expired or is invalid
+            else {
+                return res.status(403).json({message: "User not authenticated"})
+            }
+        });
+    }
+    // there has not been a log in even in this session
+    else {
+        return res.status(403).json({message: "User not logged in"})
+    }
+});
+ 
+//const PORT =5000;
+
+app.use("/customer", customer_routes);
+app.use("/", genl_routes);
+
+//app.listen(PORT,()=>console.log("Server is running "  +PORT));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
